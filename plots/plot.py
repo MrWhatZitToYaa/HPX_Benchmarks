af3d496a429f9006e42c3953dff708228dd0a561
Nikita <ichich111222333@gmail.com>
2023-10-11 18:43:33 +0200

    Optimized plots

--- plots/plot.py
+++ plots/plot.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import math
 import re
+import os
+import glob
 
-
+patternForScientificNumbers = r'([+\-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?)'
+patternForIntegers = r'[-]?\d+'
 
 def comma_to_float(valstr: str):
 	return float(valstr.decode("utf-8").replace(',','.'))
@@ -15,7 +18,6 @@ def extractDataFromLines(lines: str, patternName: str):
 
 	numberOfIdenticalLinesWithName = 0
 	lastLine = ""
-	patternForNumbers = r'([+\-]?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+\-]?\d+)?)'
 	prefix="/runtime"
 	fullPatternForLineSearches = patternName + " Vector Size"
 
@@ -23,7 +25,7 @@ def extractDataFromLines(lines: str, patternName: str):
 		# Count number of equal lines
 		if fullPatternForLineSearches in line:
 			if numberOfIdenticalLinesWithName == 0:
-				number = float(re.search(patternForNumbers, line).group())
+				number = float(re.search(patternForScientificNumbers, line).group())
 				vectorSize.append(number)
 			numberOfIdenticalLinesWithName += 1
 			lastLine = line
@@ -33,7 +35,7 @@ def extractDataFromLines(lines: str, patternName: str):
 			numberOfIdenticalLinesWithName -= 1
 			# Find the largest time
 			maxNumber = 0
-			numbers = re.findall(patternForNumbers, line)
+			numbers = re.findall(patternForScientificNumbers, line)
 			number = float(numbers[-1])
 
 			if number > maxNumber:
@@ -46,41 +48,52 @@ def extractDataFromLines(lines: str, patternName: str):
 	return vectorSize, time
 
 
-def plotting(name: str, file_path: str, plotName: str):
+def plotting(name: str, filePath: str, fileNames: str):
 	capitalName = name.capitalize()
 
-	# Extract the data
-	with open(file_path, "r") as file:
-		lines = file.readlines()
-	vectorSize, time = extractDataFromLines(lines, capitalName)
-
 	plt.figure(figsize=(16, 9))
-	plt.plot(vectorSize,
-			time,
-			marker='o',
-			linestyle='none')
-
-	# Set the size and width of axis markers and ticks for the x-axis
-	plt.xticks(fontsize=14, rotation=0)  # Adjust fontsize as needed
-	plt.tick_params(axis='x', which='both', direction='in', length=8, width=1.5)  # Adjust width as needed
-
-	# Set the size and width of axis markers and ticks for the y-axis
-	plt.yticks(fontsize=14)  # Adjust fontsize as needed
-	plt.tick_params(axis='y', which='both', direction='in', length=8, width=1.5) 
-
-	diagramName = name.capitalize()
-	plt.title(diagramName, fontsize=20)
+	plt.xticks(fontsize=14, rotation=0)
+	plt.tick_params(axis='x', which='both', direction='in', length=8, width=1.5)
+	plt.yticks(fontsize=14)
+	plt.tick_params(axis='y', which='both', direction='in', length=8, width=1.5)
+	plt.title(capitalName, fontsize=20)
 	plt.xscale("log")
 	plt.yscale("log")
 	plt.xlabel("size of vector", fontsize=14)
 	plt.ylabel("time of slowest locality [s]", fontsize=14)
 	plt.grid()
-	plt.savefig((plotName + '.png'), dpi=400)
 
-filePathTransform = "../Daniel/transform/scripts/"
-filePathReduction = "../Daniel/reduction/scripts/"
+	# Extract the data
+	for fileName in fileNames:
+		fullFilePathAndName = filePath + fileName
+		with open(fullFilePathAndName, "r") as file:
+			lines = file.readlines()
+		vectorSize, time = extractDataFromLines(lines, capitalName)
 
-plotting("transform", (filePathTransform + "4_nodes.txt"), "transform_4_localities")
-plotting("transform", (filePathTransform + "2_nodes.txt"), "transform_2_localities")
-plotting("reduction", (filePathReduction + "4_nodes.txt"), "reduction_4_localities")
-#plotting("reduction", (filePathReduction + "2_nodes.txt"), "reduction_2_localities")
+		# Create the correct label for the data
+		number = int(re.search(patternForIntegers, fileName).group())
+		labelString = str(number) + " localities"
+
+		plt.plot(vectorSize,
+				 time,
+				 marker='o',
+				 linestyle='none',
+				 label=labelString)
+	
+	plt.legend(fontsize=14)
+	plt.savefig((name + '.png'), dpi=400)
+
+def plot_all(filePath: str, name: str):
+	searchString = "_nodes.txt"
+
+	# Use glob to find all files that match the search string in the directory
+	matchingFiles = glob.glob(os.path.join(filePath, f"*{searchString}*"))
+	# Extract just the file names from the full paths
+	fileNames = [os.path.basename(file) for file in matchingFiles]
+
+	plotting(name, filePath, fileNames)
+
+
+
+plot_all("../Daniel/transform/scripts/", "transform")
+plot_all("../Daniel/reduction/scripts/", "reduction")

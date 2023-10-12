743279eaccd83f813a72188feccec68d12be7a26
Nikita <ichich111222333@gmail.com>
2023-10-12 22:34:29 +0200

    Added additional plots remove line 25 and 26 in plot.py if naming convention for scan is fixed

--- plots/plot.py
+++ plots/plot.py
@@ -21,6 +21,10 @@ def extractDataFromLines(lines: str, patternName: str):
 	prefix="/runtime"
 	fullPatternForLineSearches = patternName + " Vector Size"
 
+	# Hotfix, only tmp
+	if(fullPatternForLineSearches == "Scan Vector Size"):
+		fullPatternForLineSearches = "using vector size"
+
 	for linenumber, line in enumerate(lines):
 		# Count number of equal lines
 		if fullPatternForLineSearches in line:
@@ -48,7 +52,7 @@ def extractDataFromLines(lines: str, patternName: str):
 	return vectorSize, time
 
 
-def plotting(name: str, filePath: str, fileNames: str):
+def plottingTime(name: str, filePath: str, fileNames: str):
 	capitalName = name.capitalize()
 
 	plt.figure(figsize=(16, 9))
@@ -56,32 +60,92 @@ def plotting(name: str, filePath: str, fileNames: str):
 	plt.tick_params(axis='x', which='both', direction='in', length=8, width=1.5)
 	plt.yticks(fontsize=14)
 	plt.tick_params(axis='y', which='both', direction='in', length=8, width=1.5)
-	plt.title(capitalName, fontsize=20)
+	plt.title((capitalName + " computation times"), fontsize=20)
 	plt.xscale("log")
 	plt.yscale("log")
 	plt.xlabel("size of vector", fontsize=14)
 	plt.ylabel("time of slowest locality [s]", fontsize=14)
 	plt.grid()
 
-	# Extract the data
+	vectorSizes = []
+	times = []
+	labelStrings = []
+	# Extract the data and save it into a 2D list
 	for fileName in fileNames:
 		fullFilePathAndName = filePath + fileName
 		with open(fullFilePathAndName, "r") as file:
 			lines = file.readlines()
 		vectorSize, time = extractDataFromLines(lines, capitalName)
+		vectorSizes.append(vectorSize)
+		times.append(time)
 
 		# Create the correct label for the data
 		number = int(re.search(patternForIntegers, fileName).group())
 		labelString = str(number) + " localities"
+		labelStrings.append(labelString)
 
-		plt.plot(vectorSize,
-				 time,
+	for i in range(len(vectorSizes)):
+
+		plt.plot(vectorSizes[i],
+				 times[i],
 				 marker='o',
 				 linestyle='none',
-				 label=labelString)
+				 label=labelStrings[i])
 	
 	plt.legend(fontsize=14)
-	plt.savefig((name + '.png'), dpi=400)
+	plt.savefig((name + 'Times.png'), dpi=400)
+
+def plottingSpeedup(name: str, filePath: str, fileNames: str):
+	capitalName = name.capitalize()
+
+	plt.figure(figsize=(16, 9))
+	plt.xticks(fontsize=14, rotation=0)
+	plt.tick_params(axis='x', which='both', direction='in', length=8, width=1.5)
+	plt.yticks(fontsize=14)
+	plt.tick_params(axis='y', which='both', direction='in', length=8, width=1.5)
+	plt.title((capitalName + " speedup"), fontsize=20)
+	plt.xscale("log")
+	#plt.yscale("log")
+	plt.xlabel("size of vector", fontsize=14)
+	plt.ylabel("speedup", fontsize=14)
+	plt.grid()
+
+	vectorSizes = []
+	times = []
+	labelStrings = []
+
+	locality1VectorSize = []
+	locality1Time = []
+	# Extract the data and save it into a 2D list
+	for fileName in fileNames:
+		fullFilePathAndName = filePath + fileName
+		with open(fullFilePathAndName, "r") as file:
+			lines = file.readlines()
+		vectorSize, time = extractDataFromLines(lines, capitalName)
+		vectorSizes.append(vectorSize)
+		times.append(time)
+
+		# Create the correct label for the data
+		number = int(re.search(patternForIntegers, fileName).group())
+		labelString = str(number) + " localities"
+		labelStrings.append(labelString)
+
+		if(number == 1):
+			locality1VectorSize = vectorSize
+			locality1Time = time
+
+
+	for i in range(len(vectorSizes)):
+		speedup = [b / a for a, b in zip(times[i], locality1Time)]
+		
+		plt.plot(vectorSizes[i],
+				 speedup,
+				 marker='o',
+				 linestyle='none',
+				 label=labelStrings[i])
+	
+	plt.legend(fontsize=14)
+	plt.savefig((name + 'Speedup.png'), dpi=400)
 
 def plot_all(filePath: str, name: str):
 	searchString = "_nodes.txt"
@@ -91,9 +155,14 @@ def plot_all(filePath: str, name: str):
 	# Extract just the file names from the full paths
 	fileNames = [os.path.basename(file) for file in matchingFiles]
 
-	plotting(name, filePath, fileNames)
+	if(len(fileNames) == 0):
+		raise ValueError("Data not found")
+
+	plottingTime(name, filePath, fileNames)
+	plottingSpeedup(name, filePath, fileNames)
 
 
 
-plot_all("../Daniel/transform/scripts/", "transform")
-plot_all("../Daniel/reduction/scripts/", "reduction")
+#plot_all("../Daniel/transform/measurements/", "transform")
+#plot_all("../Daniel/reduction/measurements/", "reduction")
+plot_all("../Daniel/scan/measurements/", "scan")
